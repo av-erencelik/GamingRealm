beef3aa4baeabb1fe3070a4f18318c6aaffa1753
Mehmet Eren Çelik <erenmemo05@gmail.com>
2022-12-05 17:17:38 +0300

    added lazy loading to images

--- src/components/home/Carousel.jsx
+++ src/components/home/Carousel.jsx
@@ -1,7 +1,8 @@
-import React, { useRef, useState } from "react";
+import React, { lazy } from "react";
 // Import Swiper React components
 import { Swiper, SwiperSlide } from "swiper/react";
-
+import { LazyLoadImage } from "react-lazy-load-image-component";
+import "react-lazy-load-image-component/src/effects/blur.css";
 // Import Swiper styles
 import "swiper/css";
 import "swiper/css/pagination";
@@ -26,7 +27,7 @@ function Carousel({ games }) {
         {games.map((game) => {
           return (
             <SwiperSlide key={game.id} className="relative">
-              <img src={game.background_image}></img>
+              <LazyLoadImage src={game.background_image} effect="blur"></LazyLoadImage>
               <div className="absolute bottom-0 z-30 flex h-[50px] w-[100%] items-center justify-center bg-gray-800 bg-opacity-50">
                 <h3 className=" capitalize text-gray-200">{game.name}</h3>
               </div>
