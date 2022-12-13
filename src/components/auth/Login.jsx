ebf0b9bc25cc86ab44f8ad7fab139c26f7c7a346
av-erencelik <erenmemo05@gmail.com>
2022-12-14 00:39:18 +0300

    added validation to login component

--- src/components/auth/Login.jsx
+++ src/components/auth/Login.jsx
@@ -1,13 +1,57 @@
-import React from "react";
+import React, { useState } from "react";
+import { useFormik } from "formik";
+import * as Yup from "yup";
 
 const Login = () => {
+  const formik = useFormik({
+    initialValues: {
+      email: "",
+      password: "",
+    },
+    validationSchema: Yup.object({
+      password: Yup.string()
+        .max(15, "*Must be 15 characters or less")
+        .required("*Required")
+        .min(6, "*Must be 6 characters or more")
+        .matches(/[a-z]+/, "Must contain one lowercase character")
+        .matches(/[A-Z]+/, "Must contain one uppercase character")
+        .matches(/\d+/, "Must contain one number"),
+      email: Yup.string().email("*Invalid email address").required("*Required"),
+    }),
+    onSubmit: (values) => {
+      setError("");
+      console.log(values);
+    },
+  });
+  const [error, setError] = useState("");
   return (
-    <form className="mt-8 flex w-full flex-col gap-2">
-      <input className="rounded-md bg-gray-300 p-3 placeholder-gray-500  outline-gray-500 " placeholder="Email"></input>
+    <form className="mt-8 flex w-full flex-col gap-2" onSubmit={formik.handleSubmit}>
+      <input
+        className="rounded-md bg-gray-300 p-3 placeholder-gray-500  outline-gray-500 "
+        placeholder="Email"
+        id="email"
+        name="email"
+        type="email"
+        onChange={formik.handleChange}
+        onBlur={formik.handleBlur}
+        value={formik.values.email}
+      ></input>
+      {formik.touched.email && formik.errors.email ? (
+        <div className="text-xs text-red-600">{formik.errors.email}</div>
+      ) : null}
       <input
         className="rounded-md bg-gray-300 p-3 placeholder-gray-500 outline-gray-500  "
         placeholder="Password"
+        id="password"
+        name="password"
+        type="password"
+        onChange={formik.handleChange}
+        onBlur={formik.handleBlur}
+        value={formik.values.password}
       ></input>
+      {formik.touched.password && formik.errors.password ? (
+        <div className="text-xs text-red-600">{formik.errors.password}</div>
+      ) : null}
       <button
         type="submit"
         className="rounded-md bg-gray-300 p-3 font-semibold text-gray-800 transition-all hover:bg-gray-400"
