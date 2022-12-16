50d84657edba32287c20351ff54bd05e029e20c4
av-erencelik <erenmemo05@gmail.com>
2022-12-16 19:56:38 +0300

    added line between comments and made footer position fixed

--- src/components/game/Comment.jsx
+++ src/components/game/Comment.jsx
@@ -1,13 +1,27 @@
 import React from "react";
 const Comment = ({ index, comment }) => {
   return (
-    <div className="m-auto flex w-[95%] items-center gap-1  md:w-[70%] md:gap-3">
-      <img src={comment.photoURL} className="h-[55px] w-[55px] rounded-full object-cover"></img>
-      <div className={`mt-6 flex flex-col gap-3 ${index && "border-t-2"}  w-full border-gray-400 pt-6`}>
-        <h4 className="font-bold">{comment.username}</h4>
-        <p className="w-full">{comment.comment}</p>
+    <>
+      {index > 0 ? (
+        // line between comments
+        <div className="m-auto flex w-[95%] items-center gap-2 md:w-[70%] md:gap-3">
+          <div className="h-[55px] w-[55px]"></div>
+          <div className="w-[90%] border-t-2 border-gray-400"></div>
+        </div>
+      ) : (
+        <div className="m-auto flex w-[95%] items-center gap-2 md:w-[70%] md:gap-3">
+          <div className="h-[25px] w-[25px]"></div>
+          <div className="w-[90%]"></div>
+        </div>
+      )}
+      <div className="m-auto flex w-[95%] items-center gap-2 md:w-[70%] md:gap-3">
+        <img src={comment.photoURL} className="h-[55px] w-[55px] rounded-full object-cover"></img>
+        <div className={` flex w-[90%] flex-col gap-3 `}>
+          <h4 className="font-bold">{comment.username}</h4>
+          <p className="w-full">{comment.comment}</p>
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
